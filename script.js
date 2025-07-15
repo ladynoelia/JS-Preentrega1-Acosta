d3c3f72a13f16b9ac376b04e6536a6edad535013
Melisa Acosta <acostamelisanoelia@gmail.com>
2025-07-15 08:05:08 -0300

    Entrega final v1.0

--- script.js
+++ script.js
@@ -1,194 +1,272 @@
-class Libros {
-    constructor(id,titulo,autor,stock){
+class Materiales {
+    constructor(grup,id,titulo,stock){
+        this.grup = grup;
         this.id = id;
-        this.titulo = titulo;
-        this.autor = autor;
+        this.titulo = titulo;        
         this.stock = stock;
     }
 };
 
-const libro1 = new Libros(1, "Estructuras de hormigón armado", "Fritz Leonhardt", 0);
-const libro2 = new Libros(2, "Orgullo y prejuicio", "Jane Austen", 1);
-const libro3 = new Libros(3, "El principito", "Antoine de Saint-Exupéri", 0);
-const libro4 = new Libros(4, "Bushido, el espiritú de Japón", "Inazo Nitobe", 3);
+const mate1 = new Materiales(1, 1, "Estructuras de hormigón armado, Fritz Leonhardt", 0);
+const mate2 = new Materiales(1, 2,  "Orgullo y prejuicio, Jane Austen", 1);
+const mate3 = new Materiales(1, 3,  "El principito, Antoine de Saint-Exupéri", 0);
+const mate4 = new Materiales(1, 4,  "Bushido, el espiritú de Japón, Inazo Nitobe", 3);
+const mate5 = new Materiales(2, 5, "Proyectores", 3);
+const mate6 = new Materiales(2, 6,  "TV", 2);
+const mate7 = new Materiales(2, 7,  "Parlantes", 4);
+const mate8 = new Materiales(3, 8,  "Aula de computación", 2);
+const mate9 = new Materiales(3, 9,  "Aula de dibujo técnico", 3);
+const mate10 = new Materiales(3, 10, "Aula taller", 4);
 
-//Array de todos los libros.
-const biblioteca = [libro1, libro2, libro3, libro4];
-//Elementos del Html
-const seccionLibros = document.getElementById('cardsbox');
+//Array de todos los materiales. ----------------------------------------------------------------
+const materialDidactico = [mate1, mate2, mate3, mate4, mate5, mate6, mate7, mate8, mate9, mate10];
+
+//Elementos del Html ----------------------------------------------------------------
+const seccionTarjetas = document.getElementById('cardsbox');
 const grupoBotones = document.getElementsByClassName('filtros');
 const grupBtnReservar = document.getElementsByClassName('btn');
+const btnCancelar = document.getElementsByClassName('btn-cancel');
+const btnsMisReservas = document.getElementsByClassName('btn-mr');
 
-const disponibles = biblioteca.filter(libro => (libro.stock>0));
-//Array del carrito
+//Array de los materiales reservados ----------------------------------------------------------------
 const reservados = [];
 
-
-let soloDisponibles = false;
-function renderizarLibros(){
-    seccionLibros.innerHTML = '';        
-    biblioteca.forEach(libro => seccionLibros.innerHTML += `
+//funciones ppales ----------------------------------------------------------------
+function renderizarTarjertas(arrayDeMateriales){
+    seccionTarjetas.innerHTML = '';        
+    arrayDeMateriales.forEach(material => seccionTarjetas.innerHTML += `
         <div>
-            <h3>${libro.titulo}</h3>
-            <h4>${libro.autor}</h4>
-            <h5>Disponibles: ${libro.stock}</h5>
-            <button id='${libro.id}' class='btn' ${libro.stock === 0 ? 'disabled' : ''}>
-                ${libro.stock === 0 ? 'Sin stock' : 'Reservar'}
+            <h3>${material.titulo}</h3>
+            <h4>Disponibles: ${material.stock}</h4>
+            <button id='${material.id}' class='btn' ${material.stock === 0 ? 'disabled' : ''}>
+                ${material.stock === 0 ? 'Todo reservado' : 'Reservar'}
             </button>
         </div>`
     );
-    reservarLibro();
-    soloDisponibles = false;
-    grupoBotones[0].innerText = 'Libros disponibles';
+    reservarMaterial();
 };
-renderizarLibros();
 
-function reservarLibro() {
+renderizarTarjertas(materialDidactico);
+
+function reservarMaterial() {
     for (const button of grupBtnReservar) {
         button.addEventListener('click', () => {
-            const libroSeleccionado = biblioteca.find(libro => libro.id === parseInt(button.id));
-            if (libroSeleccionado){
-                alert(`Agregaste "${libroSeleccionado.titulo}" a la sección de Mis reservas.`);
-                reservados.push(libroSeleccionado);
+            const materialSeleccionado = materialDidactico.find(material => material.id === parseInt(button.id));
+            if (materialSeleccionado){
+                Toastify({
+                    text: `Agregaste "${materialSeleccionado.titulo}" a la sección de Mis reservas.`,
+                    duration: 3000,
+                    position: "center",
+                    gravity: "top",
+                }).showToast();                
+                reservados.push(materialSeleccionado);
             }                            
         })
     }
 };
 
-// Sección Mis reservas ----------------------------------------------------------------
-let usuarioActivo = localStorage.getItem('usuario');
-
-function guardarNombreUsuario(){
-    const nombreUsuario = prompt('Ingresa tu nombre para registrar tus pedidos');
-    if (nombreUsuario != null){
-        localStorage.setItem('usuario', nombreUsuario);
-    }    
-};
-
-function mostrarEstado(){
-    seccionLibros.innerHTML += `
-    <div class='menu_reservas'>
-        <h3>Bienvenid@</h3>
-        <h4>${usuarioActivo} estos son los libros reservados hasta en momento</h4>
-        <button class='btn_mr'>Confirmar reservaciones</button>
-        <button class='btn_mr'>Borrar todo</button>
-    </div>`
-    const btnsMisReservas = document.getElementsByClassName('btn_mr');
-    btnsMisReservas[0].addEventListener('click', () => {
-        guardarNombreUsuario();
+// Botón Biblioteca ----------------------------------------------------------------
+const biblioteca = materialDidactico.filter(material => material.grup == 1);
+grupoBotones[0].addEventListener('click', () => {
+        renderizarTarjertas(biblioteca);
     }
-    );
-    btnsMisReservas[1].addEventListener('click', () => {
-        alert('ay');
-    }
-    );
-};
+);
 
-function mostrarReservados(){    
-    reservados.forEach(libro => seccionLibros.innerHTML += `
+// Botón Audiovisual ----------------------------------------------------------------
+const audiovisual = materialDidactico.filter(material => material.grup == 2);
+grupoBotones[1].addEventListener('click', () => {
+        renderizarTarjertas(audiovisual);
+    }
+);
+
+// Botón Aulas ----------------------------------------------------------------
+const aulas = materialDidactico.filter(material => material.grup == 3);
+grupoBotones[2].addEventListener('click', () => {
+        renderizarTarjertas(aulas);
+    }
+);
+
+// Botón Mostrar todo ----------------------------------------------------------------
+grupoBotones[3].addEventListener('click', () => {
+        renderizarTarjertas(materialDidactico);
+    }
+);
+
+// Botón Mis reservas ----------------------------------------------------------------
+grupoBotones[4].addEventListener('click', () => {
+    seccionTarjetas.innerHTML = '';
+    mostrarEstado();    
+    mostrarReservados();
+    cancelarReserva();
+    confirmarReserva();
+    borrarReservados();
+    }
+);
+
+function mostrarReservados(){
+    reservados.forEach(material => seccionTarjetas.innerHTML += `
         <div>
-            <h3>${libro.titulo}</h3>
-            <h4>${libro.autor}</h4>            
-            <button id='${libro.id}' class='btn'>Cancelar reservación</button>
+            <h3>${material.titulo}</h3>
+            <h4>Cantidad: 1</h4>           
+            <button id='${material.id}' class='btn-cancel'>Cancelar reservación</button>
         </div>`
     );
 };
 
-grupoBotones[1].addEventListener ('click', () => {
-    seccionLibros.innerHTML = '';    
-    mostrarEstado();   
-    mostrarReservados();
-    }
-);
+function mostrarEstado(){
+    seccionTarjetas.innerHTML += `
+    <div class='menu_reservas'>
+        <h3>Bienvenid@</h3>
+        <h4>Estos son los materiales reservados hasta el momento</h4>
+        <button class='btn-mr'>Confirmar reservaciones</button>
+        <button class='btn-mr'>Borrar todo</button>
+    </div>`
+    
+};
 
 // Botones dentro de la sección Mis reservas --------------------------------------------------
+// Para cancelar reservas
+function cancelarReserva() {
+    for (const button of btnCancelar) {
+        button.addEventListener('click', () => {
+            const materialDeseleccionado = reservados.find(material => material.id === parseInt(button.id));
+            if (materialDeseleccionado){                
+                Toastify({
+                    text: `"${materialDeseleccionado.titulo}" eliminado de Mis reservas.`,
+                    duration: 3000,
+                    position: "center",
+                    gravity: "top",
+                }).showToast();
+                let posicionAEliminar = reservados.indexOf(materialDeseleccionado,0);
+                reservados.splice(posicionAEliminar,1);                
+            }                            
+        })
+    }    
+};
+
+// Error al reservar
+function mostrarError(){
+    Toastify({
+        text: "No hay material reservado todavía",
+        duration: 3000,
+        position: "center",
+        gravity: "top",
+    }).showToast();
+}
+
+// Para confirmar reservas
+function confirmarReserva(){
+    btnsMisReservas[0].addEventListener('click', async () => {
+        if(reservados.length == 0){
+            mostrarError();
+        } else {
+            ingresarDatos();
+        };       
+    });    
+};
+
+async function ingresarDatos(){
+    const { value: formValues } = await Swal.fire({
+        title: "Ingresá tu nombre y selecciona la fecha de uso",
+        input: "text",
+        inputLabel: "Apellido y Nombre:",
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: `
+            Continue&nbsp;<i class="fa fa-arrow-right"></i>
+        `,
+        inputValidator: (value) => {
+            if (!value) {
+                return "Es necesario colocar un nombre";
+            }
+        }
+    });    
+    if (formValues) {
+        elegirFecha();
+    }
+};
+
+async function elegirFecha(){
+    const { value: date } = await Swal.fire({
+        title: "select departure date",
+        input: "date",
+        didOpen: () => {
+            const today = (new Date()).toISOString();
+            Swal.getInput().min = today.split("T")[0];
+        },
+        inputValidator: (fecha) => {
+            if (!fecha) {
+                return "Se debe seleccionar una fecha";
+            }
+        }
+    });
+    if (date) {
+        Swal.fire({
+            title: "¡Hecho!",
+            text:"Tus materiales fueron reservados.",
+            icon: "success"
+        });
+        vaciarCarrito();
+    }
+};
+
+// Para borrar todo
+function borrarReservados(){
+    btnsMisReservas[1].addEventListener('click', () => {
+        if (reservados.length == 0){
+            mostrarError();
+        } else {
+            confirmarBorrar();
+        }        
+    })
+};
+
+function confirmarBorrar(){
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        cancelButtonText: "Cancelar",
+        confirmButtonText: "Si, borrar todo",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "¡Hecho!",
+                text: "Tus materiales fueron borrados.",
+                icon: "success"
+            });
+            vaciarCarrito();
+        }
+    });
+};
+
+function vaciarCarrito(){
+    reservados.splice(0, reservados.length);
+};
+
+//let usuarioActivo = localStorage.getItem('usuario');
+
+function guardarNombreUsuario(){
+    const nombreUsuario = document.getElementById;
+    if (nombreUsuario != null){
+        localStorage.setItem('usuario', nombreUsuario);
+    } else {
+        Toastify({
+            text: `Operación cancelada.`,
+            duration: 3000,
+            position: "center",
+            gravity: "top",
+        }).showToast();
+    }  
+};
 
 
-/*  */
 
 
 /* localStorage.removeItem('usuario'); */
 
-// Boton para mostrar solo los libros disponibles -----------------------------------------
-function mostrarDisponibles(){
-    seccionLibros.innerHTML = '';
-    grupoBotones[0].innerText = 'Mostrar todos los libros';
-    const disponibles = biblioteca.filter(libro => (libro.stock>0));
-    disponibles.forEach(libro => seccionLibros.innerHTML += `
-        <div>
-            <h3>${libro.titulo}</h3>
-            <h4>${libro.autor}</h4>
-            <h5>Disponibles: ${libro.stock}</h5>
-            <button id='${libro.id}' class='btn'>Reservar</button>
-        </div>`
-    );
-    soloDisponibles = true;
-    reservarLibro();
-};
 
-grupoBotones[0].addEventListener ('click', () => {
-    intercambiarFiltros();
-    }
-);
-function intercambiarFiltros(){
-    if(!soloDisponibles){
-        mostrarDisponibles()
-    } else {
-        renderizarLibros();
-    }
-};
-
-//
-
-
-/* grupoBotones[1].addEventListener('click', aca la funcionde agregar objetos al array de carrito renderizarLibros());
- */
-
-
-//let opcion = prompt("Selecciona un titulo de la lista\n1 - "+ libro1.titulo + ", " + libro1.autor + "\n2 - "+ libro2.titulo + ", " + libro2.autor + "\n3 - "+ libro3.titulo + ", " + libro3.autor + "\n4 - "+ libro4.titulo + ", " + libro4.autor +"\ns - Salir");
-/* 
-function reservarLibro1 () {
-    let nombreUsuario = prompt("¡El libro esta disponible!\nSi te gustaría reservarlo, ingresa tu nombre\nSino 'v' para volver al buscador");
-    if (nombreUsuario == "v"){
-        opcion = prompt("Selecciona un titulo de la lista\n1 - "+ libro1.titulo + ", " + libro1.autor + "\n2 - "+ libro2.titulo + ", " + libro2.autor + "\n3 - "+ libro3.titulo + ", " + libro3.autor + "\n4 - "+ libro4.titulo + ", " + libro4.autor +"\ns - Salir");
-    } else {
-        console.log("Genial, " + nombreUsuario + ", el libro quedó reservado, pasalo a retirar cuando puedas");
-        libro1.disponibilidad = false;
-    }
-}
-
-while (opcion != "s"){
-    switch (opcion) {
-        case "1":
-            if (libro1.disponibilidad == true){
-                reservarLibro1();                
-            } else {
-                console.log("El libro no figura en nuestro catalogo o se encuentra reservado.\nElegí otra opción.");
-            }        
-            break;
-        case "2":
-            if (libro2.disponibilidad == true){
-                reservarLibro2();                
-            } else {
-                console.log("El libro no figura en nuestro catalogo o se encuentra reservado.\nElegí otra opción.");
-            }        
-            break;
-        case "3":
-            if (libro3.disponibilidad == true){
-                reservarLibro3();                
-            } else {
-                console.log("El libro no figura en nuestro catalogo o se encuentra reservado.\nElegí otra opción.");
-            }        
-            break;
-        case "4":
-            if (libro4.disponibilidad == true){
-                reservarLibro4();
-            } else {
-                console.log("El libro no figura en nuestro catalogo o se encuentra reservado.\nElegí otra opción.");
-            }        
-            break;
-        default:
-            console.log("Opción invalida");
-    }
-    opcion = prompt("Selecciona un titulo de la lista\n1 - "+ libro1.titulo + ", " + libro1.autor + "\n2 - "+ libro2.titulo + ", " + libro2.autor + "\n3 - "+ libro3.titulo + ", " + libro3.autor + "\n4 - "+ libro4.titulo + ", " + libro4.autor +"\ns - Salir");
-} */
+//Fecha
